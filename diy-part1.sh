0616ac637fd9a00c63a209758d01b1bb26d086fc
a1an12 <34765504+a1an12@users.noreply.github.com>
2020-11-19 13:09:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,23 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# add argon theme
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+rm -rf package/lean/luci-theme-argon && mv luci-theme-argon package/lean/
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add openclash
+echo "src-git openclash https://github.com/vernesong/OpenClash;master" >> feeds.conf.default
+
+# Add clash
+# git clone -b master https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
+
+# add vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb.git
+# mv lua-maxminddb package/lean/
+# git clone https://github.com/jerrykuku/luci-app-vssr.git
+# mv luci-app-vssr package/lean/
